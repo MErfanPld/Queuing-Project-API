5515a2ffc3a56846412168ce91d2f2a6eb58de05
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2025-03-12 12:08:41 -0400

    Adding Auth and Profile

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,15 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 
-STATIC_URL = "/static/"
+STATIC_URL = '/site_statics/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
+
 # STATIC_ROOT = os.path.join(BASE_DIR, "static", "root")
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 #
