2faccd7052108c909dd5846c076495b0750e8b68
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2025-03-31 07:00:18 -0400

    edit ...

--- reservations/serializers.py
+++ reservations/serializers.py
@@ -1,29 +1,36 @@
 from rest_framework import serializers
 
+from business.models import Employee, Service
 from business.serializers import EmployeeSerializer, ServiceSerializer
 from .models import Appointment, AvailableTimeSlot
 
 
+
 class AppointmentSerializer(serializers.ModelSerializer):
     get_status = serializers.ReadOnlyField()
-    service = serializers.SerializerMethodField()
-    employee = serializers.SerializerMethodField()
+    service = ServiceSerializer(read_only=True)  # برای GET
+    employee = EmployeeSerializer(read_only=True)  # برای GET
+    service_id = serializers.PrimaryKeyRelatedField(
+        queryset=Service.objects.all(), write_only=True)  # برای POST
+    employee_id = serializers.PrimaryKeyRelatedField(
+        queryset=Employee.objects.all(), write_only=True, required=False)  # برای POST (اختیاری)
 
     class Meta:
         model = Appointment
-        fields = ['date', 'time', 'status', 'user', 'service', 'employee', 'get_status']
+        fields = [
+            'date', 'time', 'status', 'user',
+            'service', 'employee', 'get_status',
+            'service_id', 'employee_id'
+        ]
 
-    def get_service(self, obj):
-        return {'name': obj.service.name} if obj.service else None
+    def create(self, validated_data):
+        # استخراج `service_id` و `employee_id` و حذف از validated_data
+        service = validated_data.pop('service_id')
+        employee = validated_data.pop('employee_id', None)
 
-    def get_employee(self, obj):
-        if obj.employee:
-            return {
-                'first_name': obj.employee.user.first_name,
-                'last_name': obj.employee.user.last_name,
-                'skill': obj.employee.skill
-            }
-        return None
+        # ایجاد نوبت با داده‌های دریافتی
+        appointment = Appointment.objects.create(service=service, employee=employee, **validated_data)
+        return appointment
 
 
 
