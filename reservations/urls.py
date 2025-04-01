962691cfb249058663822f61860bad4b58ec993d
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2025-04-01 08:21:16 -0400

    edit ...

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,7 +4,4 @@ from .views import *
 urlpatterns = [
     path('appointments/', AppointmentListCreateView.as_view(), name='appointment-list-create'),
     path('appointments/<int:pk>/', AppointmentRetrieveUpdateDestroyView.as_view(), name='appointment-detail'),
-
-    # path('available-slots/', AvailableTimeSlotListCreateView.as_view(), name='available-slot-list'),
-    # path('available-slots/<int:pk>/', AvailableTimeSlotDetailView.as_view(), name='available-slot-detail'),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -11,36 +11,21 @@ from django.db import transaction
 # ============================== Appointment CRUD ==============================
 class AppointmentListCreateView(generics.ListCreateAPIView):
     serializer_class = AppointmentSerializer
+    permission_classes = [IsAuthenticated]  
 
     def get_queryset(self):
         user = self.request.user
-        if not user.is_authenticated:  # بررسی لاگین بودن کاربر
-            return Appointment.objects.none()  # جلوگیری از خطای AnonymousUser
-
-        if user.is_superuser:
+        if user.is_superuser: 
             return Appointment.objects.all()
-        return Appointment.objects.filter(user=user)
+        return Appointment.objects.filter(user=user)  
 
 class AppointmentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = AppointmentSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
-        if not user.is_authenticated:
-            return Appointment.objects.none()
-
         if user.is_superuser:
             return Appointment.objects.all()
         return Appointment.objects.filter(user=user)
 
-# ============================== Available Time Slot CRUD ==============================
-# class AvailableTimeSlotListCreateView(generics.ListCreateAPIView):
-#     queryset = AvailableTimeSlot.objects.all()
-#     serializer_class = AvailableTimeSlotSerializer
-#     permission_classes = [IsAuthenticated]
-#
-#
-# class AvailableTimeSlotDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = AvailableTimeSlot.objects.all()
-#     serializer_class = AvailableTimeSlotSerializer
-#     permission_classes = [IsAuthenticated]
