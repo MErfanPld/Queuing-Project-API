3f937de840cc3210cee37d82ec8af801ba518d61
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2025-04-01 08:08:53 -0400

    edit ...

--- reservations/views.py
+++ reservations/views.py
@@ -10,13 +10,28 @@ from django.db import transaction
 
 # ============================== Appointment CRUD ==============================
 class AppointmentListCreateView(generics.ListCreateAPIView):
-    queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_authenticated:  # بررسی لاگین بودن کاربر
+            return Appointment.objects.none()  # جلوگیری از خطای AnonymousUser
+
+        if user.is_superuser:
+            return Appointment.objects.all()
+        return Appointment.objects.filter(user=user)
 
 class AppointmentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
 
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_authenticated:
+            return Appointment.objects.none()
+
+        if user.is_superuser:
+            return Appointment.objects.all()
+        return Appointment.objects.filter(user=user)
 
 # ============================== Available Time Slot CRUD ==============================
 # class AvailableTimeSlotListCreateView(generics.ListCreateAPIView):
