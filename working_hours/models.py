dec16329e635e1abdb1f45d94a8e2d6c9707a837
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2025-04-24 06:38:20 -0400

    Editing Working Hours

--- working_hours/models.py
+++ working_hours/models.py
@@ -2,17 +2,7 @@ from django.db import models
 
 
 class WorkingHours(models.Model):
-    DAY_CHOICES = [
-        ('شنبه', 'شنبه'),
-        ('یکشنبه', 'یکشنبه'),
-        ('دوشنبه', 'دوشنبه'),
-        ('سه‌شنبه', 'سه‌شنبه'),
-        ('چهارشنبه', 'چهارشنبه'),
-        ('پنج‌شنبه', 'پنج‌شنبه'),
-        ('جمعه', 'جمعه'),
-    ]
-
-    day = models.CharField(max_length=10, choices=DAY_CHOICES, unique=True, verbose_name="روز")
+    day = models.DateField(unique=True,verbose_name="روز")
     opening_time = models.TimeField(verbose_name="ساعت شروع")
     closing_time = models.TimeField(verbose_name="ساعت پایان")
 
