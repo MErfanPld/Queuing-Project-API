31e5ff28f45899865e5bf6beced11b518f90d45c
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2025-10-09 19:21:25 -0400

    Add .env

--- config/settings.py
+++ config/settings.py
@@ -151,16 +151,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# STATIC_URL = 'api/static/'
+STATIC_URL = '/api/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
-# Media files (Uploaded files)
-MEDIA_URL = 'api/media/'
+MEDIA_URL = '/api/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
