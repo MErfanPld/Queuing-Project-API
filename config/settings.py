8639ce4f0996ee192cfda4798dbf0a13aac9a031
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2025-10-17 08:04:59 -0400

    ..

--- config/settings.py
+++ config/settings.py
@@ -151,12 +151,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/api/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static", "root")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/uploads/'
+MEDIA_URL = '/api/uploads/'
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static", "root")
