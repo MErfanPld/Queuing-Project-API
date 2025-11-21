5c6a224f3a0e4a59793a8db9edf0c2881555a82e
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2025-11-21 06:13:49 -0500

    db pg

--- config/settings.py
+++ config/settings.py
@@ -100,22 +100,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'narjin_app_db_9472',
+        'USER': 'narjin_usr_7821',
+        'PASSWORD': 'Ue!94n$Pq2@xL#8mZt',
     }
 }
 
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'narjin_db',
-#         'USER': 'narjin_admin',
-#         'PASSWORD': 'QtlKR~0C~aN91~b+'
-#     }
-# }
 
 
 # Password validation
